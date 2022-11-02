eee80818346b18eef7b699033f4df0fd24f35d95
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-11-02 16:54:35 -0400

    fixed players display, need reset, comp play

--- src/games/tic-tac-toe/tic-tac-toe.js
+++ src/games/tic-tac-toe/tic-tac-toe.js
@@ -2,13 +2,11 @@ import { useState } from "react"
 import styled from "styled-components"
 
 
-import startingBlock from './tic-tac-toe-images/Starting-block.png'
-import player1Img from './tic-tac-toe-images/player-1.png'
-import player2Img from './tic-tac-toe-images/player-2.png'
-
-
 const Container = styled.div`
-
+    h1{
+        margin: 15px auto 10px auto;
+        text-decoration: underline;
+    }
     .section{
         height: 500px;
         width: 500px;
@@ -19,8 +17,8 @@ const Container = styled.div`
         display: grid;
         grid-template-columns: 160px 160px 160px;
         grid-template-rows: 150px 150px 150px;
-        grid-column-gap: 20px;
-        grid-row-gap: 20px;
+        grid-column-gap: 15px;
+        grid-row-gap: 15px;
     }
 
     #one{
@@ -64,141 +62,120 @@ const Container = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
+        background-color: #ADACAC;
+        color: #FFE927;
     }
+    h2{
+        font-size: 120px;
+        text-shadow: 3px 3px 3px black;
+    }
+    h5{
+        font-size: 25px;
+        margin: 0 0 10px 0;
+    }
+    .opening{
+        margin: 30px auto;
+        border-radius: 35px;
+        height: 90px;
+        width: 180px;
+        background-color: black;
+    }
+    .head{
+        width: 500px;
+        display: inline-flex;
+        justify-content: space-between;
+    }
+    .new_game{
+        height: 30px;
+        font-size: 25px;
+        border-radius: 30px;
+        background-color: black;
 
+    }
 `
 
 
 
 function TicTacToe(){
-    const allSquares= ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+    let allSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const squares =["", "", "", "", "", "", "", "", ""]
     const players=["player1", "player2"]
 
-    const [availableSquares, setAvailableSquares]= useState(allSquares)
+    const [gameinProgress, setGameInProgress]= useState(false)
 
+    const [availableSquares, setAvailableSquares]= useState(squares)
     const [playedSquares, setPlayedSquares]= useState ([])
     const [playerTurn, setPlayerTurn] = useState("player1")
-
     const [playerOneSquares, setPlayerOneSquares]= useState([])
     const [playerTwoSquares, setPlayerTwoSquares] = useState([])
 
-    const [one, setSquare1]=useState(false)
-    const [two, setSquare2]=useState(false)
-    const [three, setSquare3]=useState(false)
-    const [four, setSquare4]=useState(false)
-    const [five, setSquare5]=useState(false)
-    const [six, setSquare6]=useState(false)
-    const [seven, setSquare7]=useState(false)
-    const [eight, setSquare8]=useState(false)
-    const [nine, setSquare9]=useState(false)
-
-
-
-    console.log("Before Playing:", availableSquares)
-    console.log("Player Turn:", playerTurn)
-    console.log("Before Playing: played squares", playedSquares)
-    console.log("Player 1 Spaces ", playerOneSquares)
-    console.log("Player 2 Spaces", playerTwoSquares)
-    console.log("Square 1", one)
-    console.log("Square 2", two)
-    console.log("Square 3", three)
-    console.log("Square 4", four)
-    console.log("Square 5", five)
-    console.log("Square 6", six)
-    console.log("Square 7", seven)
-    console.log("Square 8", eight)
-    console.log("Square 9", nine)
-
-
-
 
     
+    console.log("This is all the squares:", allSquares)
+    console.log("This is available squares:", availableSquares)
+    console.log("This is squares:", squares)
+
+
     function playingSquares(value){
-        let index = availableSquares.indexOf(value)
-        if(availableSquares.includes(value)){
+        let index = allSquares.indexOf(value)
+        if(allSquares.includes(value)){
             if(index > -1 && playerTurn === "player1"){
-                availableSquares.splice(index, 1)
+                availableSquares.splice(index, 1, "X")
                 setPlayedSquares([value, ...playedSquares])
                 setPlayerOneSquares([value, ...playerOneSquares])
                 setPlayerTurn("player2")
-                if(value === "one"){
-                    setSquare1(true)
-                }else if( value === "two"){
-                    setSquare2(true)
-                }else if( value === "three"){
-                    setSquare3(true)
-                }else if( value === "four"){
-                    setSquare4(true)
-                }else if( value === "five"){
-                    setSquare5(true)
-                }else if( value === "six"){
-                    setSquare6(true)
-                }else if( value === "seven"){
-                    setSquare7(true)
-                }else if( value === "eight"){
-                    setSquare8(true)
-                }else if( value === "nine"){
-                    setSquare9(true)
-                }
+                
             }else if (index > -1 && playerTurn === "player2"){
-                availableSquares.splice(index, 1)
+                availableSquares.splice(index, 1, "O")
                 setPlayedSquares([value, ...playedSquares])
                 setPlayerTwoSquares([value, ...playerTwoSquares])
                 setPlayerTurn("player1")
-                if(value === "one"){
-                    setSquare1(true)
-                }else if( value === "two"){
-                    setSquare2(true)
-                }else if( value === "three"){
-                    setSquare3(true)
-                }else if( value === "four"){
-                    setSquare4(true)
-                }else if( value === "five"){
-                    setSquare5(true)
-                }else if( value === "six"){
-                    setSquare6(true)
-                }else if( value === "seven"){
-                    setSquare7(true)
-                }else if( value === "eight"){
-                    setSquare8(true)
-                }else if( value === "nine"){
-                    setSquare9(true)
-                }
             }
+
         } else
         alert("This is space has already been played")
     }
 
-        function displaysquares(val){
-            if (val === false){
-                return <img  src={startingBlock} height={150} width={150} quality={100}
-                    onClick={()=> {playingSquares("val")}}
-                />
-            }else if (val === true && playerTurn === "player1"){
-                return <img src={player1Img} alt="space occupied" height={150} width={150} quality={180}/>
-            }else if (val === true && playerTurn === "player1"){
-                return <img src={player1Img} alt="space occupied" height={150} width={150} quality={180}/>
-        }
-    }
-
     return (
         <Container>
-            <h2> Tic-Tac-Toe </h2>
-            <h5> It's {playerTurn} turn</h5>
+            <h1> Tic-Tac-Toe </h1>
+            {gameinProgress?
+                <> <div className="head">
+                        <h5>Turn: {playerTurn}</h5>
+                        <button className="new_game"> New Game</button>
+                    </div> 
+                        <div className='section'>
+                            <div className="grid" >
+                                <button id="one" onClick={()=> {playingSquares(1)}}> <h2>{availableSquares[0]} </h2> </button>
+                                <button id="two" onClick={()=> playingSquares(2)}> <h2> {availableSquares[1]} </h2> </button>
+                                <button id="three" onClick={()=> playingSquares(3)}> <h2> {availableSquares[2]} </h2> </button>
+                                <button id="four" onClick={()=> playingSquares(4)}> <h2> {availableSquares[3]} </h2> </button>
+                                <button id="five" onClick={()=> playingSquares(5)}> <h2> {availableSquares[4]} </h2> </button>
+                                <button id="six" onClick={()=> playingSquares(6)}> <h2> {availableSquares[5]} </h2> </button>
+                                <button id="seven" onClick={()=> playingSquares(7)}> <h2>  {availableSquares[6]} </h2> </button>
+                                <button id="eight" onClick={()=> playingSquares(8)}> <h2> {availableSquares[7]} </h2> </button>
+                                <button id="nine" onClick={()=> playingSquares(9)}> <h2> {availableSquares[8]} </h2> </button>
+                            </div>
+                        </div>
+                    </>
+                :
+                    <><button className="opening" onClick={()=> setGameInProgress(true)}><h4>Start New Game</h4></button></>
+            }
+            {/* <h5> It's {playerTurn} turn</h5>
             <div className='section'>
                 <div className="grid" >
-                {/* <button id="one" onClick={()=> playingSquares("one")}> </button> */}
-                {displaysquares(one)}
-                <button id="two" onClick={()=> playingSquares("two")} > </button>
-                <button id="three" onClick={()=> playingSquares("three")} > </button>
-                <button id="four" onClick={()=> playingSquares("four")} > </button>
-                <button id="five" onClick={()=> playingSquares("five")} > </button>
-                <button id="six" onClick={()=> playingSquares("six")} >  </button>
-                <button id="seven" onClick={()=> playingSquares("seven")} > </button>
-                <button id="eight" onClick={()=> playingSquares("eight")} > </button>
-                <button id="nine" onClick={()=> playingSquares("nine")} > </button>
+                <button id="one" onClick={()=> {playingSquares(1)}}> <h2>{availableSquares[0]} </h2> </button>
+                <button id="two" onClick={()=> playingSquares(2)}> <h2> {availableSquares[1]} </h2> </button>
+                <button id="three" onClick={()=> playingSquares(3)}> <h2> {availableSquares[2]} </h2> </button>
+                <button id="four" onClick={()=> playingSquares(4)}> <h2> {availableSquares[3]} </h2> </button>
+                <button id="five" onClick={()=> playingSquares(5)}> <h2> {availableSquares[4]} </h2> </button>
+                <button id="six" onClick={()=> playingSquares(6)}> <h2> {availableSquares[5]} </h2> </button>
+                <button id="seven" onClick={()=> playingSquares(7)}> <h2>  {availableSquares[6]} </h2> </button>
+                <button id="eight" onClick={()=> playingSquares(8)}> <h2> {availableSquares[7]} </h2> </button>
+                <button id="nine" onClick={()=> playingSquares(9)}> <h2> {availableSquares[8]} </h2> </button>
                 </div>
-            </div>
+            </div> */}
         </Container>
     )
 }
